96a5fc8240ac0f93c8501cfa72c37c29c734bbbb
s0r3-glitch <ggoodson2432@gmail.com>
2024-07-12 12:25:06 -0700

    update stuff

--- lua/s0r3fig.lua
+++ lua/s0r3fig.lua
@@ -2,17 +2,6 @@ local function open_nvim_tree()
   -- open the tree
   require("nvim-tree.api").tree.open()
 end
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
 vim.keymap.set("", "<up>", "<nop>", { noremap = true })
 vim.keymap.set("", "<down>", "<nop>", { noremap = true })
 vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
@@ -21,7 +10,7 @@ vim.opt.mousescroll = "ver:0,hor:0"
 vim.opt.mouse = ""
 
 vim.opt.rtp:prepend(lazypath)
-require("vim-options")
-require("lazy").setup("plugins")
+require("s0r3fig").vim-options
+require("s0r3fig.plugins")
 --vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 require'lspconfig'.pyright.setup{}
