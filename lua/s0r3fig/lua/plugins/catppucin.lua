37c21f316d170651ec63dae31c7372e82f2a5baa
s0r3-glitch <ggoodson2432@gmail.com>
2024-07-12 12:54:31 -0700

    update

--- lua/s0r3fig/lua/plugins/catppucin.lua
+++ lua/s0r3fig/lua/plugins/catppucin.lua
@@ -4,7 +4,7 @@ return{
 		name = "catppuccin",
 		priority = 1000,
        config = function()
-            require("lua.plugins.configs.catppuccin-config")
+            require("s0r3fig.lua.plugins.configs.catppuccin-config")
             vim.cmd.colorscheme "catppuccin-macchiato"
         end
 
