9b7deacc668db79dbcc486d4d9cf6e43e386fcdf
s0r3-glitch <ggoodson2432@gmail.com>
2024-07-12 14:26:07 -0700

    add a fwe more window movement commands

--- lua/s0r3fig/lua/vim-options.lua
+++ lua/s0r3fig/lua/vim-options.lua
@@ -9,4 +9,6 @@ vim.cmd('nnoremap("<C-S-Right>", "<cmd>:vertical resize -1<cr>", "Minimize windo
 vim.cmd('nnoremap("<C-S-Left>", "<cmd>:vertical resize +1<cr>", "Maximize window")')
 vim.cmd("nnoremap <C-H> <cmd>:wincmd h<cr>") 
 vim.cmd("nnoremap <C-L> <cmd>:wincmd l<cr>") 
+vim.cmd("nnoremap <C-K> <cmd>:wincmd k<cr>") 
+vim.cmd("nnoremap <C-J> <cmd>:wincmd j<cr>") 
 vim.cmd("nnoremap <C-/> <cmd>:WhichKey<cr>") 
