2432ae45843e428e0fde933e79a1a1ba80cdaee9
geygo9 <geoffrey.goodson@lfg.com>
2024-08-15 10:45:05 -0400

    forgot the return

--- lua/s0r3fig/lua/plugins/dap-ui.lua
+++ lua/s0r3fig/lua/plugins/dap-ui.lua
@@ -1,1 +1,4 @@
-{ "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+return {
+
+    { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+}
